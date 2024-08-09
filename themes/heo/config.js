1d347f914fd7cc96e56d3af96aad96a4359ff932
msrefs <refs@aliyun.com>
2024-08-09 13:06:28 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -23,9 +23,9 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://neotec.us.kg',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '基本作品', url: '/tag/基本作品' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: 'Basic', url: '/tag/Basic' },
+  HEO_HERO_CATEGORY_2: { title: 'Popular', url: '/tag/Popular' },
+  HEO_HERO_CATEGORY_3: { title: 'Tutorials', url: '/tag/Tutorials' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -35,11 +35,9 @@ const CONFIG = {
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '专修交互与设计',
+    '数码科技爱好者',
+    '团队小组发动机'
   ],
 
   // 个人资料底部按钮
