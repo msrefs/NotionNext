bd105bddac3a2044c552abe383d544f923156d43
msrefs <refs@aliyun.com>
2024-11-02 12:25:26 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,16 +20,16 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_3: 'NEOTEC.US.KG',
+  HEO_HERO_TITLE_4: '更新已完成',
+  HEO_HERO_TITLE_5: 'NotionNext4.0 基于Nextjs 4.0',
+  HEO_HERO_TITLE_LINK: 'https://github.com/msrefs/NotionNext',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: 'Random Jump',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: 'Basic', url: '/tag/Basic' },
-  HEO_HERO_CATEGORY_2: { title: 'Recommend', url: '/tag/Recommend' },
+  HEO_HERO_CATEGORY_2: { title: 'Popular', url: '/tag/Popular' },
   HEO_HERO_CATEGORY_3: { title: 'Tutorials', url: '/tag/Tutorials' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
