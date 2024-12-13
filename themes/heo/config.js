a9dd563fcee05ae8d53961c4abcb4a4d84c2724b
msrefs <refs@aliyun.com>
2024-12-13 19:30:19 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到Neotec.', url: '' },
-    { title: 'Neotec创造无限可能', url: '' }
+    { title: '欢迎来到Neotec.', url: '/about' },
+    { title: 'Neotec创造无限可能', url: '/about' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -43,7 +43,7 @@ const CONFIG = {
   ],
 
   // 个人资料底部按钮
-  HEO_INFO_CARD_URL1: '/about'',
+  HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-circle-info',
   HEO_INFO_CARD_URL2: '/javalibrary',
   HEO_INFO_CARD_ICON2: 'fas fa-book',
