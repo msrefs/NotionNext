58a11c162153fc8a57db1830758288263ba5f63c
msrefs <refs@aliyun.com>
2025-03-29 16:25:56 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -9,9 +9,9 @@ export default function PoweredBy(props) {
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
-        href='https://github.com/tangly1024/NotionNext'
+        href='https://nextjs.org/'
         className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
+        Nextjs 11.4.5
       </a>
       .
     </div>
