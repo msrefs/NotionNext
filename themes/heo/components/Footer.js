d536e84fa233106ed87f19b5e2070a35ec5037a3
msrefs <refs@aliyun.com>
2025-03-29 17:23:17 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -9,7 +9,7 @@ import SocialButton from './SocialButton'
  */
 const Footer = () => {
   const BEI_AN = siteConfig('BEI_AN')
-  const beianlink = siteconfig("BEI_AN_LINK')
+  const beianlink = siteconfig('BEI_AN_LINK')
   const BIO = siteConfig('BIO')
   return (
     <footer className='relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'>
