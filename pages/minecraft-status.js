0c56095b11188fef8656507e67125deb2f7857cf
msrefs <refs@aliyun.com>
2025-04-04 19:46:39 +0800

    Delete pages/minecraft-status.js

--- pages/minecraft-status.js
+++
@@ -1,9 +0,0 @@
-import MinecraftStatus from '../components/MinecraftStatus';
-
-export default function StatusPage() {
-  return (
-    <div style={{ maxWidth: '600px', margin: '0 auto' }}>
-      <MinecraftStatus />
-    </div>
-  );
-}
