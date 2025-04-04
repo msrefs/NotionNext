1914d7a6b58061a84176a05b8e006389e121c5a0
msrefs <refs@aliyun.com>
2025-04-04 16:29:23 +0800

    Create minecraft-status.js

---
+++ pages/minecraft-status.js
@@ -0,0 +1,9 @@
+import MinecraftStatus from '../components/MinecraftStatus';
+
+export default function StatusPage() {
+  return (
+    <div style={{ maxWidth: '600px', margin: '0 auto' }}>
+      <MinecraftStatus />
+    </div>
+  );
+}
