3a12319a347c5633f00e462d50dc703e4e2fb42d
msrefs <refs@aliyun.com>
2025-04-05 21:29:24 +0800

    Update InfoCard.js

--- themes/heo/components/InfoCard.js
+++ themes/heo/components/InfoCard.js
@@ -28,12 +28,11 @@ export function InfoCard(props) {
       <div className='flex justify-between'>
         {/* 问候语 */}
         <GreetingsWords />
-        {/* 头像 */}
+        {/* 头像 - 移除了圆形遮罩 */}
         <div
           className={`${isSlugPage ? 'absolute right-0 -mt-8 -mr-6 hover:opacity-0 hover:scale-150 blur' : 'cursor-pointer'} justify-center items-center flex transform transitaion-all duration-200`}>
           <LazyImage
             src={siteInfo?.icon}
-            className='rounded-full'
             width={isSlugPage ? 100 : 28}
             alt={siteConfig('AUTHOR')}
           />
@@ -70,50 +69,4 @@ export function InfoCard(props) {
   )
 }
 
-/**
- * 了解更多按鈕
- * @returns
- */
-function MoreButton() {
-  const url3 = siteConfig('HEO_INFO_CARD_URL3', null, CONFIG)
-  const text3 = siteConfig('HEO_INFO_CARD_TEXT3', null, CONFIG)
-  if (!url3) {
-    return <></>
-  }
-  return (
-    <Link href={url3}>
-      <div
-        className={
-          'group bg-indigo-400 hover:bg-white text-white hover:text-[#4f65f0] flex items-center transition-colors duration-200 py-2 px-3 rounded-full space-x-1'
-        }>
-        <ArrowRightCircle
-          className={
-            'group-hover:stroke-[#4f65f0] w-6 h-6 transition-all duration-100'
-          }
-        />
-        <div className='font-bold'>{text3}</div>
-      </div>
-    </Link>
-  )
-}
-
-/**
- * 欢迎语
- */
-function GreetingsWords() {
-  const greetings = siteConfig('HEO_INFOCARD_GREETINGS', null, CONFIG)
-  const [greeting, setGreeting] = useState(greetings[0])
-  // 每次点击，随机获取greetings中的一个
-  const handleChangeGreeting = () => {
-    const randomIndex = Math.floor(Math.random() * greetings.length)
-    setGreeting(greetings[randomIndex])
-  }
-
-  return (
-    <div
-      onClick={handleChangeGreeting}
-      className='select-none cursor-pointer py-1 px-2 bg-indigo-400 hover:bg-white hover:text-[#4f65f0] text-sm rounded-lg duration-200 transition-colors'>
-      {greeting}
-    </div>
-  )
-}
+// ... MoreButton 和 GreetingsWords 组件保持不变 ...
